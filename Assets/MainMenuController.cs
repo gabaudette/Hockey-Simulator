7b917b9335be2993510d1737d1485e6f0e57d728
Gabriel Audette <gaudette@recursyve.io>
2022-03-20 15:11:43 -0400

    Added Context Menu and MainMenuController

---
+++ Assets/MainMenuController.cs
@@ -0,0 +1,53 @@
+using System;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+
+// Todo @GA create Abstract controller
+public class MainMenuController : MonoBehaviour
+{
+    private VisualElement _root;
+
+    private void Awake()
+    {
+        _root = GetComponent<UIDocument>().rootVisualElement;
+
+        Debug.Log(_root, this);
+
+        var playButton = _root.Q<Button>("play-button");
+
+        playButton.RegisterCallback<ClickEvent>(ShowContextMenu);
+    }
+
+    private void OnDestroy()
+    {
+        foreach (var visualElement in _root.Children())
+        {
+            visualElement.UnregisterCallback<ClickEvent>(ShowContextMenu);
+            visualElement.UnregisterCallback<ClickEvent>(LoadScene);
+        }
+    }
+
+    private static void ShowContextMenu(ClickEvent e)
+    {
+        if (!(e.currentTarget is VisualElement button)) return;
+
+        if (button.parent.childCount == 1) return;
+
+        var contextMenu = button.parent.Q("mode-selection-context-menu");
+        
+        contextMenu.visible = !contextMenu.visible;
+        
+        foreach (var subMenuButton in contextMenu.Children())
+        {
+            subMenuButton.RegisterCallback<ClickEvent>(LoadScene);
+        }
+    }
+
+    private static void LoadScene(ClickEvent e)
+    {
+        if (!(e.currentTarget is VisualElement button)) return;
+        
+        // TODO GA Go to Scene
+    }
+}
